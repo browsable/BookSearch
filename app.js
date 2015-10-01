3c46a907643e64ead6469f0a374ce146654f7b87
browsable <hernia@koreatech.ac.kr>
2015-10-01 19:26:57 +0900

    setting

--- app.js
+++ app.js
@@ -1,26 +1,64 @@
-/**
- * Created by admin on 2015-08-12.
- */
+var express = require('express');
+var path = require('path');
+var favicon = require('serve-favicon');
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
 
-    // 1. 필요한 module require
-    var http = require("http"),
-        express = require("express"),
-        app = express(),
-        path = require("path"),
-        bodyParser = require("body-parser");
+var routes = require('./routes/index');
+var users = require('./routes/users');
 
-    // 2. routing module require
-    var routes = require("./routes");
+var app = express();
+//port setup
+app.set('port',process.env.PORT || 9000);
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
 
-    // 3. view setting
-    app.set("views",path.join(__dirname, "views"));
-    app.set("view engine", "ejs");
+// uncomment after placing your favicon in /public
+//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
 
-    // 4. express setting
-    app.use(express.static(path.join(__dirname,"public")));
-    app.use(bodyParser());
-    app.use("/",routes);
+app.use('/', routes);
+app.use('/users', users);
 
-    // 5. 서버 생성 후 기동
-    http.createServer(app).listen(5000,"localhost");
-    console.log("서버기동 - 5000");
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
+
+// error handlers
+
+// development error handler
+// will print stacktrace
+if (app.get('env') === 'development') {
+  app.use(function(err, req, res, next) {
+    res.status(err.status || 500);
+    res.render('error', {
+      message: err.message,
+      error: err
+    });
+  });
+}
+
+// production error handler
+// no stacktraces leaked to user
+app.use(function(err, req, res, next) {
+  res.status(err.status || 500);
+  res.render('error', {
+    message: err.message,
+    error: {}
+  });
+});
+
+//creates Server
+module.exports = app;
+var server = app.listen(app.get('port'), function(){
+  console.log('Express server listening on port' +server.address().port);
+});
